2793cccb11f867ff20718c7e70ba9f6f173b3da3
Shayash Jena <shayash2203@gmail.com>
2021-06-26 12:24:39 +0530

    26 june

--- 04. Recursion & Backtracking/Recursion/011remDupl.cpp
+++ 04. Recursion & Backtracking/Recursion/011remDupl.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
+string remDupl(string s){
+    if(s.length()==0){
+        return "";
+    }
+
+    char ch=s[0];
+    string ans = remDupl(s.substr(1));
+    if(ch==ans[0]){
+        return ans;
+    }
+    return ch+ans;
+}
+
 int main(){
-     
+    string s = "aaaabbbeeecdddd";
+    cout<<remDupl(s)<<endl;
     return 0;
 }
