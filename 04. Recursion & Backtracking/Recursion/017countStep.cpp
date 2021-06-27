067f68349a206d77a5c1baf1d8c85ffceba76157
Shayash Jena <shayash2203@gmail.com>
2021-06-27 16:47:50 +0530

    27 June

---
+++ 04. Recursion & Backtracking/Recursion/017countStep.cpp
@@ -0,0 +1,25 @@
+// Count the number of paths possible from start point to end point in grameboard
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPath(int s, int e){
+    if(s==e){
+        return 1;
+    }
+    if(s>e){
+        return 0;
+    }
+
+    int count=0;
+    for(int i=1;i<=6;i++){
+        count+=countPath(s+i,e);
+    }
+    return count;
+}
+
+int main(){
+    cout<<countPath(0,3);
+ 
+    return 0;
+}
