70d058c554163b0cf72edcd8322939e6da597589
shayash22 <shayash2203@gmail.com>
2021-07-22 11:40:18 +0530

    22 July

---
+++ 06. Linked List/002insertHead.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+void insertAtHead(node* &head, int val){
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+}
+
+
+void display(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+int main(){
+
+    node* head=NULL;
+    insertAtHead(head,1);
+    insertAtHead(head,2);
+    insertAtHead(head,3);
+    display(head);
+ 
+    return 0;
+}
