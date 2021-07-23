acad194f46ff8eaad788b8c340b8398fe5a6ae10
shayash22 <shayash2203@gmail.com>
2021-07-23 19:08:27 +0530

    23 July

---
+++ 06. Linked List/005revAlt.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+
+void display(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+node* reverse(node* &head){
+    node* prevptr=NULL;
+    node* currptr=head;
+    node* nextptr;
+
+    while(currptr!=NULL){
+        nextptr=currptr->next;
+        currptr->next=prevptr;
+
+        prevptr=currptr;
+        currptr=nextptr;
+    }
+
+    return prevptr;
+
+}
+
+
+void insertAtTail(node* &head, int val){
+    node* n= new node(val);
+
+    if(head==NULL){
+        head=n;
+        return;
+    }
+
+    node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+
+    }
+    temp->next=n;
+}
+
+int main(){
+
+    node* head=NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    display(head);
+
+    node* newhead= reverse(head);
+    display(newhead);
+ 
+    return 0;
+}
